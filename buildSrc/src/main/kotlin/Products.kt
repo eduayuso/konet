b236ab29ec4b25a186883cd54449eb56acea7556
EduAyuso <eduayuso@gmail.com>
2020-10-14 11:16:24 +0200

    Upgraded to 1.1.2

--- buildSrc/src/main/kotlin/Products.kt
+++ buildSrc/src/main/kotlin/Products.kt
@@ -3,13 +3,13 @@ object Products {
     object Konet {
 
         const val groupId = "dev.eduayuso.kolibs"
-        const val version = "1.1.1"
+        const val version = "1.1.2"
     }
 
     object SampleApp {
 
         const val appId = "dev.eduayuso.kolibs.samples.konet.users"
         const val versionCode = 1
-        const val versionName = "1.1.1"
+        const val versionName = "1.1.2"
     }
 }
